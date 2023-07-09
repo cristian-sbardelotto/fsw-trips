2c865b38300f927569c528ea0f2d4e4b5440194e
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-07-09 11:30:30 -0300

    🎨 style(Homepage): added responsiveness

--- src/components/RecommendedTrips.tsx
+++ src/components/RecommendedTrips.tsx
@@ -2,7 +2,7 @@ import { Trip } from '@prisma/client';
 import TripItem from './TripItem';
 
 export default async function RecommendedTrips() {
-  const trips: Trip[] = await fetch(`${process.env.HOST_URL}api/trips`).then(
+  const trips: Trip[] = await fetch('http://localhost:3000/api/trips').then(
     response => response.json()
   );
 
@@ -18,7 +18,7 @@ export default async function RecommendedTrips() {
         <div className='w-full h-[1px] bg-gray-light' />
       </div>
 
-      <div className='flex flex-col items-center mt-5 gap-5'>
+      <div className='flex flex-col items-center mt-5 gap-5 lg:mt-12 lg:flex-row lg:flex-wrap lg:justify-center lg:gap-10'>
         {trips.map(trip => (
           <TripItem
             trip={trip}
--- src/components/TripSearch.tsx
+++ src/components/TripSearch.tsx
@@ -26,12 +26,12 @@ export default function TripSearch() {
   }
 
   return (
-    <div className='container mx-auto p-5 bg-world-map bg-cover bg-center bg-no-repeat'>
-      <h1 className='font-semibold text-2xl text-primary-dark text-center'>
+    <div className='container mx-auto p-5 bg-world-map bg-cover bg-center bg-no-repeat lg:py-28'>
+      <h1 className='font-semibold text-2xl text-primary-dark text-center lg:text-[2.5rem]'>
         Encontre sua próxima <span className='text-primary'>viagem!</span>
       </h1>
 
-      <div className='flex flex-col gap-4 mt-5'>
+      <div className='flex flex-col gap-4 mt-5 lg:flex-row lg:max-w-[948px] lg:mx-auto lg:p-4 lg:bg-primary lg:mt-12 lg:bg-opacity-20 lg:rounded-lg'>
         <Input
           placeholder='Onde você quer ir?'
           error={!!errors.text}
@@ -44,7 +44,7 @@ export default function TripSearch() {
           errorMessage={errors.text?.message}
         />
 
-        <div className='flex gap-4'>
+        <div className='flex gap-4 lg:w-full'>
           <Controller
             name='startDate'
             control={control}
@@ -74,7 +74,12 @@ export default function TripSearch() {
           />
         </div>
 
-        <Button onClick={() => handleSubmit(onSubmit)()}>Buscar</Button>
+        <Button
+          onClick={() => handleSubmit(onSubmit)()}
+          className='lg:w-1/2 w-full'
+        >
+          Buscar
+        </Button>
       </div>
     </div>
   );
