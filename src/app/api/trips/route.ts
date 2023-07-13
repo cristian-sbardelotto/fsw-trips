2bceccf046b1010b20ef33044a5fc5195ba7e152
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-07-13 10:35:39 -0300

    ♻️ refactor(Home): changed query to fetch only recommended trips

--- src/app/api/trips/route.ts
+++ src/app/api/trips/route.ts
@@ -2,7 +2,11 @@ import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const trips = await prisma.trip.findMany();
+  const trips = await prisma.trip.findMany({
+    where: {
+      recommended: true,
+    },
+  });
 
   return new NextResponse(JSON.stringify(trips), { status: 200 });
 }
